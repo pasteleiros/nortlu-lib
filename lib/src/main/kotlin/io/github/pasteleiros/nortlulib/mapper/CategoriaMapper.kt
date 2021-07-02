5d5b07bc4dd2ef06ad6888741a2cdd44d14dc160
Guilherme Vicente <guilhermedovicente@gmail.com>
2021-07-02 18:57:41 -0400

    Adicionando mapper para listas no categoria mapper

--- lib/src/main/kotlin/io/github/pasteleiros/nortlulib/mapper/CategoriaMapper.kt
+++ lib/src/main/kotlin/io/github/pasteleiros/nortlulib/mapper/CategoriaMapper.kt
@@ -5,3 +5,5 @@ import io.github.pasteleiros.nortlulib.entity.CategoriaEntity
 
 fun CategoriaDto.toEntity() = CategoriaEntity(this.id, this.descricao)
 fun CategoriaEntity.toDto() = CategoriaDto(this.id, this.descricao)
+fun List<CategoriaDto>.toListEntity():List<CategoriaEntity> = this.map{ e -> e.toEntity()}
+fun List<CategoriaEntity>.toListDto():List<CategoriaDto> = this.map{ e -> e.toDto()}
