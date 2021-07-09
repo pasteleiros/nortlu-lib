a27ed4f15da1a173555e63de9aa6af8bbf04d86f
ricardo <ricardo.rodrigues.alves@hotmail.com>
2021-07-09 20:56:04 -0300

    Commitando lib

--- lib/src/main/kotlin/io/github/pasteleiros/nortlulib/exception/Exceptions.kt
+++ lib/src/main/kotlin/io/github/pasteleiros/nortlulib/exception/Exceptions.kt
@@ -1,6 +1,8 @@
 package io.github.pasteleiros.nortlulib.exception
 
-open class BaseException()
-class NaoEncontradoException():BaseException()
-class ServicoException():BaseException()
-class ValidacaoException():BaseException()
+import kotlin.Exception
+
+open class BaseException(msg : String = "Ocorreu um erro inesperado"):Exception(msg)
+class NaoEncontradoException(msg : String):BaseException(msg)
+class ServicoException(msg : String):BaseException(msg)
+class ValidacaoException(msg : String):BaseException(msg)
